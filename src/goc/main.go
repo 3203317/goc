fa28a79e5a5a2ef748f455ae48267e5f172f9797
speedt <13837186852@qq.com>
2017-12-25 10:31:44 +0800

    haha

--- src/goc/main.go
+++ src/goc/main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"config"
 	"fmt"
+	_ "golang.org/x/net/websocket"
+	"io"
+	"log"
+	"net/http"
 )
+
+func def(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
 
 func main() {
 	config.LoadCfg()
 	fmt.Println("Hello, GO!")
+
+	http.HandleFunc("/", def)
+	err := http.ListenAndServe(":80", nil)
+	if nil != err {
+		log.Fatal(err)
+	}
+
 }
