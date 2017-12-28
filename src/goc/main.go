e13c2f62053249888755441ed46c64db7cd804e6
speedt <13837186852@qq.com>
2017-12-28 17:15:46 +0800

    haha

--- src/goc/main.go
+++ src/goc/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"config"
 	"fmt"
+	"github.com/go-redis/redis"
 	_ "golang.org/x/net/websocket"
 	"io"
 	"log"
@@ -17,10 +18,29 @@ func main() {
 	config.LoadCfg()
 	fmt.Println("Hello, GO!")
 
-	http.HandleFunc("/", def)
-	err := http.ListenAndServe(":80", nil)
+	client := redis.NewClient(&redis.Options{
+		Addr:     "47.104.99.102:6379",
+		Password: "123456",
+		DB:       1,
+	})
+
+	// pong, err := client.Ping().Result()
+	// if nil != err {
+	// 	log.Fatal(err)
+	// }
+
+	// log.Println(pong)
+
+	vals, err := client.EvalSha("a0ad12f31d7de75a5153bdff954caf5bc15b9501", []string{"1", "1", "backend_1", "123456"}, 5, 68, "BACK").Result()
 	if nil != err {
 		log.Fatal(err)
 	}
 
+	log.Println(vals)
+
+	// http.HandleFunc("/", def)
+	// err := http.ListenAndServe(":80", nil)
+	// if nil != err {
+	// 	log.Fatal(err)
+	// }
 }
